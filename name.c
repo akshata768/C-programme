cf774c4ac3e7b1c0e537b5856efabb88cb19d019
akshata768 <akshatamuguli82@gmail.com>
2025-05-13 14:03:31 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    char name[] = "Your Name";
+    int age = 25;
+    char location[] = "Your Location";
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Location: %s\n", location);
+
+    return 0;
+}

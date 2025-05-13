f8e8714ba4fbf23133e797b4528bada2b55a8183
akshata768 <akshatamuguli82@gmail.com>
2025-05-13 21:02:50 +0530

    Create marks.c

---
+++ marks.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    
+    printf("Enter the student's marks: ");
+    scanf("%d", &marks);
+
+    
+    if (marks < 0 || marks > 100) {
+        printf("Invalid marks. Please enter a value between 0 and 100.\n");
+        return 1;
+    }
+
+    if (marks >= 50) {
+        printf("The student passed.\n");
+    } else {
+        printf("The student failed.\n");
+    }
+
+    return 0;
+}

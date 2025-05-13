f65ce85ddd793d30022504ce85f66461a5dff770
akshata768 <akshatamuguli82@gmail.com>
2025-05-13 21:41:09 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (number >= 10 && number <= 100) {
+        printf("The number %d is within the range 10 to 100.\n", number);
+    } else {
+        printf("The number %d is NOT within the range 10 to 100.\n", number);
+    }
+
+    return 0;
+}

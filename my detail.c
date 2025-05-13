862fa1f24dd0882bb65ed0e683058d7147a10243
akshata768 <akshatamuguli82@gmail.com>
2025-05-13 15:42:09 +0530

    Create my detail.c

---
+++ my detail.c
@@ -0,0 +1,17 @@
+// function defnition
+#include <stdio.h>
+
+int shridevi()
+{
+printf("akshata\n");
+printf("date of birth\n");
+printf("age\n");
+printf("phone number\n");
+
+}
+int main()
+
+{
+shridevi();
+return 0;
+}

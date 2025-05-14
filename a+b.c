80773590d639e6995a3a1d1fba9db50dd5d0f3de
akshata768 <akshatamuguli82@gmail.com>
2025-05-14 19:08:18 +0530

    Create a+b.c

---
+++ a+b.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int sum(int a,int b){
+ return a+b;   
+}
+int main(){
+    int x=10, y=29;
+    printf("sum:%d\n",sum(x,y));
+    return 0;
+}

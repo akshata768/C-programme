1dc26574685f171e0726bf58f002c3cf5a4ac26f
akshata768 <akshatamuguli82@gmail.com>
2025-05-15 09:42:14 +0530

    Create square.c

---
+++ square.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+int sq(int n){
+    return n*n;
+}
+int main(){
+    int n=2;
+    int result=sq( n);
+    printf("ahdr\n");
+    printf("the square of  %d is %d\n",n,result);
+
+return 0;
+}

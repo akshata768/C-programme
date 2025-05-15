27916a7e75757a87ecc24f8b158123c7b8a738d3
akshata768 <akshatamuguli82@gmail.com>
2025-05-15 19:56:17 +0530

    Create fgets.c

---
+++ fgets.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+char main() {
+char name[20];
+printf("you name:%c\n");
+fgets(name,20,stdin);
+printf("the name is=%c",name);
+return 0;
+}
